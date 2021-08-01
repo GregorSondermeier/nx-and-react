80276e16df89400ec87215dcec099823699d75ae
Gregor Sondermeier <gregor.sondermeier.github@gmail.com>
2021-08-01 15:48:05 +0200

    affected

--- stores/apps/bike-store/src/app/app.spec.tsx
+++ stores/apps/bike-store/src/app/app.spec.tsx
@@ -11,6 +11,6 @@ describe('App', () => {
   it('should have a greeting as the title', () => {
     const { getByText } = render(<App />);
 
-    expect(getByText('Welcome to bike-store!')).toBeTruthy();
+    expect(getByText('Welcome to bike-store - some changes!')).toBeTruthy();
   });
 });
--- stores/apps/bike-store/src/app/app.tsx
+++ stores/apps/bike-store/src/app/app.tsx
@@ -9,7 +9,7 @@ export function App() {
       <Header />
       <header className="flex">
         <Logo width="75" height="75" />
-        <h1 data-test="greeting">Welcome to bike-store!</h1>
+        <h1 data-test="greeting">Welcome to bike-store - some changes!</h1>
       </header>
       <main>
         <h2>Resources &amp; Tools</h2>
